5c192e38ad16c008beb069a6168e3ba398fafcdf
vsrmkpgit <79241681+vsrmkpgit@users.noreply.github.com>
2021-04-27 14:42:02 +0530

    Add files via upload
    esp32 arduino file

---
+++ ESP32.ino
@@ -0,0 +1,16 @@
+void setup() 
+{
+    Serial.begin(115200); 
+    Serial.println("Interfacfing arduino with Esp32");
+    Serial.println("Receiving data to Esp32");
+}
+
+void loop() 
+{
+    
+   float a = analogRead(25);
+   float b = ((a*5*3.3)/4095)+0.6;
+    Serial.print("Voltage:");
+    Serial.println(b);
+    delay(500);
+} 
