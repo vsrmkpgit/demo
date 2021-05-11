18e1797d79930db431d8bd1ec0acce1c8d346cef
sri varshini <srivarshini@vsrmkp.com>
2021-05-11 12:02:39 +0530

    changes made

--- ESP32.ino
+++ ESP32.ino
@@ -1,16 +1,15 @@
 void setup() 
 {
     Serial.begin(115200); 
-    Serial.println("Interfacfing arduino with Esp32");
+    Serial.println("Interfacfing Arduino with Esp32");
     Serial.println("Receiving data to Esp32");
 }
 
 void loop() 
 {
-    
    float a = analogRead(25);
    float b = ((a*5*3.3)/4095)+0.6;
     Serial.print("Voltage:");
     Serial.println(b);
-    delay(500);
+    delay(100);
 } 
